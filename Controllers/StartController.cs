b80baf9ea88694d9a40d832e2bd7ddf7cc3f172f
Klenisson Mateus <klenissonmateuspessoal@gmail.com>
2023-08-03 23:20:46 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/StartController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace mycommerce.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StartController : ControllerBase
+    {
+
+        
+    }
+}
